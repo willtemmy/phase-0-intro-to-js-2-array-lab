d69231384fd1a35075e725d8789736818c65279c
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-14 18:41:02 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    let newcat = [...cats, name];
+    return newcat;
+}
+
+function prependCat(name){
+    let newcat = [name,...cats];
+    return newcat;
+}
+
+function removeLastCat(){
+    let newcat = cats.slice(0,cats.length - 1);
+    return newcat;
+}
+
+function removeFirstCat(){
+    let newcat = cats.slice(1);
+    return newcat;
+}
+
